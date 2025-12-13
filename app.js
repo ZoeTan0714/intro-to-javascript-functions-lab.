82769305821db3bbff459826ec502a1b18a45a14
zoetan <zoe.txy@outlook.com>
2025-12-13 22:35:18 +0800

    commit change 2

--- app.js
+++ app.js
@@ -29,12 +29,86 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-
-const isAult (number) {
-    if(number >= 18) {
-        console.log('Adult')
+//Exercise 2, Method 1
+const isAdult = (number) => {
+    if (number >= 18) {
+        return 'Adult';
     } else {
-        console.log('Minor')
+        return 'Minor';
     }
 }
 console.log('Exercise 2 Result:', isAdult(21));
+
+//Exercise 2, Method 2
+function isAdult2(Age) {
+    if (Age>=18) {
+        console.log('Exercise 2 Result: Adult2');
+        return;
+    }
+    console.log('Exercise 2 Result: Minor2');
+}
+isAdult2 (21);
+    
+
+//Exercise 2, Method 3
+function isAdult3(Age) {
+return Age >= 18 ? 'Exercise 2 Result: Adult3' : 'Exercise 2 Result: Minor3';
+}
+console.log (isAdult3 (21));
+
+
+//Exercise 2, Method 4
+function isAdult4(age) {
+    if (age>=18) {
+        return 'Exercise 2 Result: Adult4';}
+        return 'Exercise 2 Result: Minor4';
+}
+let result = isAdult4(21);
+console.log(result);
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+// Exercise 3, method 1
+const isCharAVowel = char => {
+    if (char === 'y') {
+        return 'false'
+    }
+    return 'true'
+}
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+// Exercise 3, method 2
+function isCharAVowel2 (char) {
+    return char === "y" ? "Exercise 3 Result: false" : "Exercise 3 Result: true";
+}
+console.log(isCharAVowel2 ("a"));
+
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
